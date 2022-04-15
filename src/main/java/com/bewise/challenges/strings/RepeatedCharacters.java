e4174abefb6c5406a8d775f4f4d4991f2ed91f2c
Manuel_LAguilar <manuel.mlaaguilar@gmail.com>
2022-04-15 17:42:41 -0300

    Added RepeatedCharacters solution

--- src/main/java/com/bewise/challenges/strings/RepeatedCharacters.java
+++ src/main/java/com/bewise/challenges/strings/RepeatedCharacters.java
@@ -1,5 +1,10 @@
 package com.bewise.challenges.strings;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class RepeatedCharacters {
 
     /**
@@ -13,7 +18,27 @@ public class RepeatedCharacters {
      * @return booleano indicando si la cadena cumple con las propiedades
      */
     public Boolean isValid(String cadena) {
-        return false;
+        HashMap<Character, Integer> values = new HashMap<Character, Integer>();
+
+        // Contar las apariciones
+        for(int i = 0; i < cadena.length(); i ++){
+            values.put(cadena.charAt(i), values.getOrDefault(cadena.charAt(i), 0) + 1);
+        }
+        ArrayList<Integer> appearances = values.values().stream().distinct().collect(Collectors.toCollection(ArrayList::new));
+        //Verificar condiciones
+        //Si hay mas de dos cantidades de caracteres ya no es valido
+        if(appearances.size() > 2) {return false;}
+        //Si hay dos pero las cantidades tienen una diferencia de > 1 entre ellas entonces no es valido
+        //Si la diferencia entre cantidades es menor o igual a uno entonces es valido
+        else if  (appearances.size() == 2){
+            return Math.abs(appearances.get(0) - appearances.get(1)) <= 1;
+        }
+        // Si paso las otras condiciones es valido
+        else{
+            return true;
+        }
+
+
     }
 }
 
